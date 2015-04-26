b1b89fc36c349352c787ad62830317b68677316d
Gavin Kistner <gavin@phrogz.net>
2015-04-26 13:55:14 -0600

    Add statistics when running tests.

--- test/test_neatjson.js
+++ test/test_neatjson.js
@@ -1,4 +1,6 @@
 var neatJSON = require('../javascript/neatjson.js').neatJSON;
+var startTime = new Date;
+var count=0, pass=0;
 require('./tests.js').tests.forEach(function(valTest){
 	var value = valTest.value;
 	valTest.tests.forEach(function(test){
@@ -12,12 +14,17 @@ require('./tests.js').tests.forEach(function(valTest){
 			var json = neatJSON(value);
 		}
 		var success = (test.json.constructor==RegExp) ? test.json.test(json) : json==test.json;
-		if (!success){
+		if (success) pass+=1;
+		else{
 			console.log(mesg+")");
 			console.log('EXPECTED');
 			console.log(test.json);
 			console.log('ACTUAL');
 			console.log(json,"\n");
 		}
+		count+=1;
 	});
 });
+var elapsed = (new Date)-startTime;
+console.log(pass+"/"+count+" test"+(count==1 ? '' : 's')+" passed in "+elapsed.toFixed(2)+"ms ("+(count/elapsed*1000).toFixed(0)+" tests per second)");
+
--- test/test_neatjson.rb
+++ test/test_neatjson.rb
@@ -1,15 +1,22 @@
 require '../lib/neatjson'
 require './tests'
 
+start = Time.now
+pass  = 0
+count = 0
 TESTS.each do |value_tests|
 	val, tests = value_tests[:value], value_tests[:tests]
 	tests.each do |test|
 		begin
+			count += 1
 			json = test[:opts] ? JSON.neat_generate(val,test[:opts].dup) : JSON.neat_generate(val)
 			mesg = test[:opts] ? "JSON.neat_generate(#{val.inspect},#{test[:opts].inspect})" : "JSON.neat_generate(#{val.inspect})"
 			raise "#{mesg}\nEXPECTED:\n#{test[:json]}\nACTUAL:\n#{json}\n\n" unless test[:json]===json
+			pass += 1
 		rescue StandardError => e
 			puts e
 		end
 	end
 end
+elapsed = Time.now-start
+puts "%d/%d test#{:s if count!=1} passed in %.2fms (%d tests per second)" % [pass, count, elapsed*1000, count/elapsed]
