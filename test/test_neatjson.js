8f215d929ddb9e46a746340c4d2bfa5daaf72530
Gavin Kistner <gavin@phrogz.net>
2015-10-12 10:02:11 -0600

    JSON test runner gracefully catches and reports runtime errors

--- test/test_neatjson.js
+++ test/test_neatjson.js
@@ -4,23 +4,28 @@ var count=0, pass=0;
 require('./tests.js').tests.forEach(function(valTest){
 	var value = valTest.value;
 	valTest.tests.forEach(function(test){
-		var mesg = "neatJSON("+JSON.stringify(value);
-		if (test.opts){
-			var opts = {};
-			for (var k in test.opts) opts[k] = test.opts[k];
-			var json = neatJSON(value,opts);
-			mesg += ","+JSON.stringify(test.opts);
-		}else{
-			var json = neatJSON(value);
-		}
-		var success = (test.json.constructor==RegExp) ? test.json.test(json) : json==test.json;
-		if (success) pass+=1;
-		else{
-			console.log(mesg+")");
-			console.log('EXPECTED');
-			console.log(test.json);
-			console.log('ACTUAL');
-			console.log(json,"\n");
+		var cmd = "neatJSON("+JSON.stringify(value)+(test.opts?","+JSON.stringify(test.opts):'')+")";
+		try{
+			if (test.opts){
+				var opts = {};
+				for (var k in test.opts) opts[k] = test.opts[k];
+				var json = neatJSON(value,opts);
+			}else{
+				var json = neatJSON(value);
+			}
+			var success = (test.json.constructor==RegExp) ? test.json.test(json) : json==test.json;
+			if (success) pass+=1;
+			else{
+				console.log(cmd);
+				console.log('EXPECTED');
+				console.log(test.json);
+				console.log('ACTUAL');
+				console.log(json,"\n");
+			}
+		} catch (e){
+			console.log("Error running "+cmd);
+			console.log(e.stack);
+			console.log("")
 		}
 		count+=1;
 	});
