8a8392e29cfa98d58e7f3713baf24484fa1c6e73
Gavin Kistner <gavin@phrogz.net>
2016-04-14 09:38:47 -0600

    JS lib supports objects without Object constructor (e.g. location)

--- javascript/neatjson.js
+++ javascript/neatjson.js
@@ -39,74 +39,70 @@ function neatJSON(value,opts){
 	function rawBuild(o,indent){
 		if (o===null || o===undefined) return indent+'null';
 		else{
-			switch(o.constructor){
-				case Number:
-					var isFloat = (o === +o && o !== (o|0));
-					return indent + ((isFloat && ('decimals' in opts)) ? o.toFixed(opts.decimals) : (o+''));
-
-				case Array:
-					if (!o.length) return indent+"[]";
-					var pieces  = o.map(function(v){ return build(v,'') });
-					var oneLine = indent+'['+apad+pieces.join(comma)+apad+']';
-					if (opts.wrap===false || oneLine.length<=opts.wrap) return oneLine;
-					if (opts.short){
-						var indent2 = indent+' '+apad;
-						pieces = o.map(function(v){ return build(v,indent2) });
-						pieces[0] = pieces[0].replace(indent2,indent+'['+apad);
-						pieces[pieces.length-1] = pieces[pieces.length-1]+apad+']';
-						return pieces.join(',\n');
-					}else{
-						var indent2 = indent+opts.indent;
-						return indent+'[\n'+o.map(function(v){ return build(v,indent2) }).join(',\n')+'\n'+(opts.indentLast?indent2:indent)+']';
-					}
-
-				case Object:
+			if (typeof o==='number'){
+				var isFloat = (o === +o && o !== (o|0));
+				return indent + ((isFloat && ('decimals' in opts)) ? o.toFixed(opts.decimals) : (o+''));
+			}else if (o instanceof Array){
+				if (!o.length) return indent+"[]";
+				var pieces  = o.map(function(v){ return build(v,'') });
+				var oneLine = indent+'['+apad+pieces.join(comma)+apad+']';
+				if (opts.wrap===false || oneLine.length<=opts.wrap) return oneLine;
+				if (opts.short){
+					var indent2 = indent+' '+apad;
+					pieces = o.map(function(v){ return build(v,indent2) });
+					pieces[0] = pieces[0].replace(indent2,indent+'['+apad);
+					pieces[pieces.length-1] = pieces[pieces.length-1]+apad+']';
+					return pieces.join(',\n');
+				}else{
+					var indent2 = indent+opts.indent;
+					return indent+'[\n'+o.map(function(v){ return build(v,indent2) }).join(',\n')+'\n'+(opts.indentLast?indent2:indent)+']';
+				}
+			}else if (o instanceof Object){
+				var keyvals=[],i=0;
+				for (var k in o) keyvals[i++] = [JSON.stringify(k), build(o[k],'')];
+				if (!keyvals.length) return indent+'{}';
+				if (opts.sorted) keyvals = keyvals.sort(function(kv1,kv2){ kv1=kv1[0]; kv2=kv2[0]; return kv1<kv2?-1:kv1>kv2?1:0 });
+				keyvals = keyvals.map(function(kv){ return kv.join(colon1) }).join(comma);
+				var oneLine = indent+"{"+opad+keyvals+opad+"}";
+				if (opts.wrap===false || oneLine.length<opts.wrap) return oneLine;
+				if (opts.short){
 					var keyvals=[],i=0;
-					for (var k in o) keyvals[i++] = [JSON.stringify(k), build(o[k],'')];
-					if (!keyvals.length) return indent+'{}';
+					for (var k in o) keyvals[i++] = [indent+' '+opad+JSON.stringify(k),o[k]];
 					if (opts.sorted) keyvals = keyvals.sort(function(kv1,kv2){ kv1=kv1[0]; kv2=kv2[0]; return kv1<kv2?-1:kv1>kv2?1:0 });
-					keyvals = keyvals.map(function(kv){ return kv.join(colon1) }).join(comma);
-					var oneLine = indent+"{"+opad+keyvals+opad+"}";
-					if (opts.wrap===false || oneLine.length<opts.wrap) return oneLine;
-					if (opts.short){
-						var keyvals=[],i=0;
-						for (var k in o) keyvals[i++] = [indent+' '+opad+JSON.stringify(k),o[k]];
-						if (opts.sorted) keyvals = keyvals.sort(function(kv1,kv2){ kv1=kv1[0]; kv2=kv2[0]; return kv1<kv2?-1:kv1>kv2?1:0 });
-						keyvals[0][0] = keyvals[0][0].replace(indent+' ',indent+'{');
-						if (opts.aligned){
-							var longest = 0;
-							for (var i=keyvals.length;i--;) if (keyvals[i][0].length>longest) longest = keyvals[i][0].length;
-							var padding = repeat(' ',longest);
-							for (var i=keyvals.length;i--;) keyvals[i][0] = padRight(padding,keyvals[i][0]);
-						}
-						for (var i=keyvals.length;i--;){
-							var k=keyvals[i][0], v=keyvals[i][1];
-							var indent2 = repeat(' ',(k+colonN).length);
-							var oneLine = k+colonN+build(v,'');
-							keyvals[i] = (opts.wrap===false || oneLine.length<=opts.wrap || !v || typeof v!="object") ? oneLine : (k+colonN+build(v,indent2).replace(/^\s+/,''));
-						}
-						return keyvals.join(',\n') + opad + '}';
-					}else{
-						var keyvals=[],i=0;
-						for (var k in o) keyvals[i++] = [indent+opts.indent+JSON.stringify(k),o[k]];
-						if (opts.sorted) keyvals = keyvals.sort(function(kv1,kv2){ kv1=kv1[0]; kv2=kv2[0]; return kv1<kv2?-1:kv1>kv2?1:0 });
-						if (opts.aligned){
-							var longest = 0;
-							for (var i=keyvals.length;i--;) if (keyvals[i][0].length>longest) longest = keyvals[i][0].length;
-							var padding = repeat(' ',longest);
-							for (var i=keyvals.length;i--;) keyvals[i][0] = padRight(padding,keyvals[i][0]);
-						}
-						var indent2 = indent+opts.indent;
-						for (var i=keyvals.length;i--;){
-							var k=keyvals[i][0], v=keyvals[i][1];
-							var oneLine = k+colonN+build(v,'');
-							keyvals[i] = (opts.wrap===false || oneLine.length<=opts.wrap || !v || typeof v!="object") ? oneLine : (k+colonN+build(v,indent2).replace(/^\s+/,''));
-						}
-						return indent+'{\n'+keyvals.join(',\n')+'\n'+(opts.indentLast?indent2:indent)+'}'
+					keyvals[0][0] = keyvals[0][0].replace(indent+' ',indent+'{');
+					if (opts.aligned){
+						var longest = 0;
+						for (var i=keyvals.length;i--;) if (keyvals[i][0].length>longest) longest = keyvals[i][0].length;
+						var padding = repeat(' ',longest);
+						for (var i=keyvals.length;i--;) keyvals[i][0] = padRight(padding,keyvals[i][0]);
 					}
-
-				default:
-					return indent+JSON.stringify(o);
+					for (var i=keyvals.length;i--;){
+						var k=keyvals[i][0], v=keyvals[i][1];
+						var indent2 = repeat(' ',(k+colonN).length);
+						var oneLine = k+colonN+build(v,'');
+						keyvals[i] = (opts.wrap===false || oneLine.length<=opts.wrap || !v || typeof v!="object") ? oneLine : (k+colonN+build(v,indent2).replace(/^\s+/,''));
+					}
+					return keyvals.join(',\n') + opad + '}';
+				}else{
+					var keyvals=[],i=0;
+					for (var k in o) keyvals[i++] = [indent+opts.indent+JSON.stringify(k),o[k]];
+					if (opts.sorted) keyvals = keyvals.sort(function(kv1,kv2){ kv1=kv1[0]; kv2=kv2[0]; return kv1<kv2?-1:kv1>kv2?1:0 });
+					if (opts.aligned){
+						var longest = 0;
+						for (var i=keyvals.length;i--;) if (keyvals[i][0].length>longest) longest = keyvals[i][0].length;
+						var padding = repeat(' ',longest);
+						for (var i=keyvals.length;i--;) keyvals[i][0] = padRight(padding,keyvals[i][0]);
+					}
+					var indent2 = indent+opts.indent;
+					for (var i=keyvals.length;i--;){
+						var k=keyvals[i][0], v=keyvals[i][1];
+						var oneLine = k+colonN+build(v,'');
+						keyvals[i] = (opts.wrap===false || oneLine.length<=opts.wrap || !v || typeof v!="object") ? oneLine : (k+colonN+build(v,indent2).replace(/^\s+/,''));
+					}
+					return indent+'{\n'+keyvals.join(',\n')+'\n'+(opts.indentLast?indent2:indent)+'}'
+				}
+			}else{
+				return indent+JSON.stringify(o);
 			}
 		}
 	}
@@ -126,6 +122,6 @@ function neatJSON(value,opts){
 		return (str + pad).substring(0, pad.length);
 	}
 }
-neatJSON.version = "0.7";
+neatJSON.version = "0.7.2";
 
 })(typeof exports === 'undefined' ? this : exports);
