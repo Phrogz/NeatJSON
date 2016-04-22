b72faf3128d7f918b98b8caa1542306d3be28c1e
Gavin Kistner <gavin@phrogz.net>
2016-04-22 11:08:26 -0600

    Remove all string mutation (workaround OpalRB limitation)

--- lib/neatjson.rb
+++ lib/neatjson.rb
@@ -25,7 +25,7 @@ module JSON
 	# @option opts [Integer] :around_colon_1  (0) Number of spaces to put before/after colons for single-line objects.
 	# @option opts [Integer] :before_colon_1  (0) Number of spaces to put before colons for single-line objects.
 	# @option opts [Integer] :after_colon_1   (0) Number of spaces to put after colons for single-line objects.
-	# @option opts [Integer] :around_colon_n  (0) Number of spaces to put before/after colons for multi-line objects.
+	# @option opts [Integer] :aroun_n  (0) Number of spaces to put before/after colons for multi-line objects.
 	# @option opts [Integer] :before_colon_n  (0) Number of spaces to put before colons for multi-line objects.
 	# @option opts [Integer] :after_colon_n   (0) Number of spaces to put after colons for multi-line objects.
 	# @return [String] the JSON representation of the object.
@@ -33,6 +33,7 @@ module JSON
 	# The lambda for the `sort` option will be passed the string name of the key, the value, and the hash for the object being sorted.
 	# The values returned for all keys must be all comparable, or an error will occur.
 	def self.neat_generate(object,opts={})
+		opts ||= {}
 		opts[:wrap] = 80 unless opts.key?(:wrap)
 		opts[:wrap] = -1 if opts[:wrap]==true
 		opts[:indent]         ||= "  "
@@ -81,8 +82,8 @@ module JSON
 						elsif opts[:short]
 							indent2 = "#{indent} #{apad}"
 							pieces = o.map{ |v| build[ v,indent2 ] }
-							pieces[0].sub! indent2, "#{indent}[#{apad}"
-							pieces.last << apad << "]"
+							pieces[0] = pieces[0].sub indent2, "#{indent}[#{apad}"
+							pieces[pieces.length-1] = "#{pieces.last}#{apad}]"
 							pieces.join ",\n"
 						else
 							indent2 = "#{indent}#{opts[:indent]}"
@@ -111,10 +112,11 @@ module JSON
 						else
 							if opts[:short]
 								keyvals = o.map{ |k,v| ["#{indent} #{opad}#{k.to_s.inspect}",v] }
-								keyvals[0][0].sub! "#{indent} ", "#{indent}{"
+								keyvals[0][0] = keyvals[0][0].sub "#{indent} ", "#{indent}{"
 								if opts[:aligned]
 									longest = keyvals.map(&:first).map(&:length).max
-									keyvals.each{ |k,v| k.replace( "%-#{longest}s" % k ) }
+									formatk = "%-#{longest}s"
+									keyvals.map!{ |k,v| [ formatk % k,v] }
 								end
 								keyvals.map! do |k,v|
 									indent2 = " "*"#{k}#{colonn}".length
@@ -125,13 +127,14 @@ module JSON
 										one_line
 									end
 								end
-								keyvals.join(",\n") << opad << "}"
+								"#{keyvals.join(",\n")}#{opad}}"
 							else
 								keyvals = o.map{ |k,v| ["#{indent}#{opts[:indent]}#{k.to_s.inspect}",v] }
 								keyvals = keyvals.sort_by(&:first) if opts[:sorted]
 								if opts[:aligned]
 									longest = keyvals.map(&:first).map(&:length).max
-									keyvals.each{ |k,v| k.replace( "%-#{longest}s" % k ) }
+									formatk = "%-#{longest}s"
+									keyvals.map!{ |k,v| [ formatk % k,v] }
 								end
 								indent2 = "#{indent}#{opts[:indent]}"
 								keyvals.map! do |k,v|
@@ -155,4 +158,3 @@ module JSON
 		build[object,'']
 	end
 end
-
