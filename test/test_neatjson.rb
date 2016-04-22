bcbd99e549685e1565c66823093add465655a3fb
Gavin Kistner <gavin@phrogz.net>
2016-04-22 11:02:36 -0600

    Add perf-testing mode for A/B perf comparisons

--- test/test_neatjson.rb
+++ test/test_neatjson.rb
@@ -1,13 +1,15 @@
 require_relative '../lib/neatjson'
 require_relative './tests'
 
-start = Time.now
 pass  = 0
 count = 0
+$perftesting = %w[-p --perfest].include?(ARGV.first)
+$large = JSON.parse(IO.read(File.expand_path('../large.json',__FILE__))) if $perftesting
+start = Time.now
 TESTS.each do |value_tests|
 	val, tests = value_tests[:value], value_tests[:tests]
 	tests.each do |test|
-		cmd = test[:opts] ? "JSON.neat_generate(#{val.inspect},#{test[:opts].inspect})" : "JSON.neat_generate(#{val.inspect})"
+		cmd = "JSON.neat_generate(#{val.inspect},#{test[:opts].inspect})"
 		begin
 			json = test[:opts] ? JSON.neat_generate(val,test[:opts].dup) : JSON.neat_generate(val)
 			raise "EXPECTED:\n#{test[:json]}\nACTUAL:\n#{json}" unless test[:json]===json
@@ -16,6 +18,9 @@ TESTS.each do |value_tests|
 			puts "Error running #{cmd}", e, ""
 		end
 		count += 1
+		if $perftesting
+			JSON.neat_generate($large,test[:opts]) rescue puts "Error serializing $large with #{test[:opts].inspect}"
+		end
 	end
 end
 elapsed = Time.now-start
